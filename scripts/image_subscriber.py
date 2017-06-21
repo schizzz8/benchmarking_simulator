ec78df371e60a3f4cf37806ebb417f62034c409a
schizzz8 <fnardi@dis.uniroma1.it>
2017-06-21 16:38:53 +0200

    added stub and filter functions

--- scripts/image_subscriber.py
+++ scripts/image_subscriber.py
@@ -7,6 +7,7 @@ roslib.load_manifest('benchmarking_simulator')
 import sys
 import rospy
 import cv2
+import numpy as np
 
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
@@ -24,27 +25,48 @@ class image_subscriber:
     self.depth_image_sub = message_filters.Subscriber('depth_image', Image)
     self.rgb_image_sub = message_filters.Subscriber('rgb_image', Image)
 
-    self.ts = message_filters.ApproximateTimeSynchronizer([self.depth_image_sub,self.rgb_image_sub],10,0.3)
+    self.ts = message_filters.ApproximateTimeSynchronizer([self.depth_image_sub,self.rgb_image_sub],10,0.1)
     self.ts.registerCallback(self.callback)
 
   def callback(self,depth_data,rgb_data):
     try:
-      cv_image = self.bridge.imgmsg_to_cv2(rgb_data, "bgr8")
+      cv_depth_image = self.bridge.imgmsg_to_cv2(depth_data,desired_encoding="8UC1")
+      cv_rgb_image = self.bridge.imgmsg_to_cv2(rgb_data,desired_encoding="bgr8")
     except CvBridgeError as e:
       print(e)
 
+    result=self.stub(cv_rgb_image)
 
-    cv2.imshow("Image window", cv_image)
-    cv2.waitKey(3)
+    self.filter(cv_depth_image,result)
+    
+  def stub(self,image):
+    #cv2.imshow("Image window",image)
+    #cv2.waitKey(30)
+    #rospy.loginfo("image size: %d",image.size)
 
-    try:
-      self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
-    except CvBridgeError as e:
-      print(e)
+    output=[0,1,0,50,100,50,100]
+    return output
+
+
+  def filter(self,image,result):
+    roi=image[result[3]:result[4],result[5]:result[6]]
+    #cv2.imshow("Image window",roi)
+    #cv2.waitKey(30)
+    rospy.loginfo("roi image size: %s",str(roi.shape))
+    
+    max_depth=np.amax(roi)
+    #rospy.loginfo("max depth: %d",max_depth)
+    
+    ret,output=cv2.threshold(image,max_depth,255,cv2.THRESH_TOZERO_INV)
+    #cv2.imshow("Image window",output)
+    #cv2.waitKey(30)
+    #rospy.loginfo("output image size: %s",str(output.shape))
+    
+    return output
 
 def main(args):
-  im_sub = image_subscriber()
   rospy.init_node('image_subscriber', anonymous=True)
+  im_sub = image_subscriber()
   try:
     rospy.spin()
   except KeyboardInterrupt:
